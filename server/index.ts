c62147338f772d0b9f4d21a74046f9f232e88039
Paul Cohen <ctannugipaul@gmail.com>
2025-05-29 13:53:40 +0200

    final prep for vercel deployment

--- server/index.ts
+++ server/index.ts
@@ -1,6 +1,9 @@
+import dotenv from "dotenv";
+dotenv.config();
 import express, { type Request, Response, NextFunction } from "express";
 import { registerRoutes } from "./routes";
 import { setupVite, serveStatic, log } from "./vite";
+import { dot } from "node:test/reporters";
 
 const app = express();
 app.use(express.json());
@@ -60,11 +63,7 @@ app.use((req, res, next) => {
   // this serves both the API and the client.
   // It is the only port that is not firewalled.
   const port = 5000;
-  server.listen({
-    port,
-    host: "0.0.0.0",
-    reusePort: true,
-  }, () => {
-    log(`serving on port ${port}`);
-  });
+ server.listen(port, "localhost", () => {
+  log(`serving on http://localhost:${port}`);
+});
 })();
